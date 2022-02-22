bca51c323d0325af8db3f2d25690f114a3434e96
christijohnson <97490948+christijohnson@users.noreply.github.com>
2022-02-22 16:01:45 -0700

    updated+

--- Greed_game/game/casting/artifact.py
+++ Greed_game/game/casting/artifact.py
@@ -1,5 +1,4 @@
 from game.casting.actor import Actor
-from game.shared.point import Point
 
 
 class Artifact(Actor):
@@ -12,7 +11,7 @@ class Artifact(Actor):
     '''
 
     def __init__(self):
-        super().__init__() #super links to the parnet class
+        super().__init__() 
         self._score = 0
         
 
@@ -33,23 +32,3 @@ class Artifact(Actor):
         Tell the director to remove the artifact when 'touched'.
         """
         pass
-
-
-
-    # def get_score(self):
-    #     """
-    #     This gets the total points the change in point value either pos or neg
-    #     """
-    #     return self._score
-  
-    # def move_next(self, max_x, max_y):
-    #     """Moves the actor to its next position according to its velocity. Will wrap the position 
-    #     from one side of the screen to the other when it reaches the given maximum x and y values.
-        
-    #     Args:
-    #         max_x (int): The maximum x value.
-    #         max_y (int): The maximum y value.
-    #     """
-    #     x = (self._position.get_x() + self._velocity.get_x()) % max_x
-    #     y = (self._position.get_y() + self._velocity.get_y()+0) % max_y
-    #     self._position = Point(x, y)
