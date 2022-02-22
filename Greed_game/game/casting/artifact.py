40dc9b11a012ce0c722a6844ad4bf8d685e45f92
dh5harris <dh5harris@hotmail.com>
2022-02-21 18:51:34 -0600

    cleaned up code; added scoring

--- Greed_game/game/casting/artifact.py
+++ Greed_game/game/casting/artifact.py
@@ -1,35 +1,37 @@
 from game.casting.actor import Actor
 
+
 class Artifact(Actor):
     '''
-    Grabs message from message.txt
+    Calculates the score for a gem or a rock.
 
     Attributes: 
-    _message (string): from the message text file
+     set_score is to detemine a point for a gem, minus a point for a rock.
+     remove_artifact is to remove the artifact when the robot touches it.
     '''
 
     def __init__(self):
         super().__init__() #super links to the parnet class
         self._score = 0
+        
 
 
-    def set_score(self, text):
+    def set_score(self):
         """
         Tell director to add or minus 1 based on artifact type
         return 
         """
-        if text == 42:
-            score += 1
-        elif text == 79:
-            score -=1
-        else:
-            score += 0
-        self._score = score
-
-    def get_score(self):
-        """
-        This gets the total points the change in point value either pos or neg
-        """
+        if self._text == '*':
+            self._score += 1
+        elif self._text  == 'O':
+            self._score -=1
         return self._score
-  
+
+    def remove_artifact(self):
+        """
+        Tell the director to remove the artifact when 'touched'.
+        """
+        pass
+
+
 
